8a7d7349431864c64ddd6dfe8823c9e99e4a9191
Alex1445085 <n1445085@yandex.ru>
2024-05-27 15:19:00 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome! ");
+        System.out.println("Hello and welcome! ");
+        // 1.
+        byte varByte = 127;
+        short varShort = 32000;
+        int varInt = 1000987;
+        long varLong = 9888777666L;
+        float varFloat = 12.88765f;
+        double varDouble = 3.141592652;
+        System.out.println("Значение переменной 'varByte' с типом 'byte' равно " + varByte);
+        System.out.println("Значение переменной 'varChar' с типом 'char' равно " + varShort);
+        System.out.println("Значение переменной 'varInt' с типом 'int' равно " + varInt);
+        System.out.println("Значение переменной 'varLong' с типом 'long' равно " + varLong);
+        System.out.println("Значение переменной 'varFloat' с типом 'float' равно " + varFloat);
+        System.out.println("Значение переменной 'varDouble' с типом 'double' равно " + varDouble);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
